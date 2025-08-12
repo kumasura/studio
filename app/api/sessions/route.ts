aec2282cbb7a8f4210ae3a66592769496c4e9427
kumasura <suraj.kumar.2013@gmail.com>
2025-08-12 08:43:29 +0530

    Create route.ts

---
+++ app/api/sessions/route.ts
@@ -0,0 +1,7 @@
+export const runtime = 'edge'
+import { createSession } from '@/lib/runtime'
+
+export async function POST() {
+  const id = createSession()
+  return new Response(JSON.stringify({ session_id: id }), { headers: { 'content-type': 'application/json' } })
+}
